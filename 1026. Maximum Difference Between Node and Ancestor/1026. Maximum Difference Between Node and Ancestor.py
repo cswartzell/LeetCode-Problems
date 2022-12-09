7c082bb2b9dc75818ec293c5a80f7a13d6c93fa8
Cameron Swartzell <csswartzell@gmail.com>
2022-12-08 18:23:03 -0800

    12-07-2022

---
+++ 1026. Maximum Difference Between Node and Ancestor/1026. Maximum Difference Between Node and Ancestor.py
@@ -0,0 +1,31 @@
+# 12-08-2022 Leetcode 1026. Maximum Difference Between Node and Ancestor
+# https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/description/
+
+
+# If Im not mistaken, this is a DFS where we track min and max
+# seen so far in the traversal, then, when at a leaf node
+# return the abs of the difference. We should check max
+# on every call so we keep the LARGETS difference
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        def DFS(root, amax, amin, vdif):
+            if not root:
+                return vdif
+            vdif = max([vdif, abs(amax - root.val), abs(amin - root.val)])
+            amax = max(amax, root.val)
+            amin = min(amin, root.val)
+            return max(
+                DFS(root.right, amax, amin, vdif), DFS(root.left, amax, amin, vdif)
+            )
+
+        return DFS(root, root.val, root.val, 0)
