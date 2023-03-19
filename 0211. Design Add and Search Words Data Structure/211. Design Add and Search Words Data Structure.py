1976b4ae809c43aa449b1f1e7cff80a6845b06bc
Cameron Swartzell <csswartzell@gmail.com>
2023-03-18 23:57:21 -0700

    03-18-2023

--- 0211. Design Add and Search Words Data Structure/211. Design Add and Search Words Data Structure.py
+++ 0211. Design Add and Search Words Data Structure/211. Design Add and Search Words Data Structure.py
@@ -1,16 +1,25 @@
+# 03-18-2023 Leetcode 211. Design Add and Search Words Data Structure
+# https://leetcode.com/problems/design-add-and-search-words-data-structure/description/
+
 # Well, immediately I see two was to handle this. Kind of 3.
-# The trivial one to implement is to simply store a hashmap, like a set.
-# We could go a bit more advanced and roll our own hash, disucuss the merits
-# and intricacies in selecting a hashing algo, colisions etc.
+#We COULD store a simple set and for any '.' in a word, we add
+#all 26 possible vals to our set. There can be up to 3 dots however
+#and 26^3 = 17576 possible words in a three dot word. Our set will be
+# too big. 
 
-# Or we can make a Trie. Trie is most compact and "fancy", plus memory
-# efficient. Because we include wild card chars trie makes this simpler
+#We can use REGEX to simply match within a set, this is trivial too.
 
+#Or we can make a Trie. Trie is most compact and "fancy", plus memory
+#efficient.
 
+#Lets go for trie for the practice:
+# Hooray! Worked first try. Slow as shit, barely passing
+ 
 # class WordDictionary:
 #     def __init__(self):
 #         self.words = dict()
 #         self.final = False
+#         self.longest = 0
 
 #     def addWord(self, word: str) -> None:
 #         if not word:
@@ -30,37 +39,26 @@
 #         else:
 #             return False
 
-import re
-import string
-
-class WordDictionary:
-    def __init__(self):
-        self.words = set()
-    
-    def addWord(self, word: str) -> None:
-        self.words.add(word)
-
-    def search(self, word: str) -> bool:
-        if word in self.words:
-            return True
-        #Pretty sure I am being EXTRA here. Can just use the dot notation DIRECTLY for a regex search
-        #This matches valid words, but we didnt put any such restricions on the words in the first place
-        # regx = "".join(ltr if ltr in string.ascii_letters else "[a-zA-Z]" for ltr in word)
-        re_word = re.compile("\A"+word+"$")
-        return re.search(re_word, s_word) for s_word in self.words
-
-
-
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
 # obj.addWord(word)
 # param_2 = obj.search(word)
 
-wordD = WordDictionary()
-wordD.addWord("dad")
-wordD.addWord("bad")
-wordD.addWord("mad")
-print(wordD.search("pad"))
-print(wordD.search("bad"))
-print(wordD.search(".ad"))
-print(wordD.search("b.."))
+
+
+# # # REGEX
+# import re
+
+# class WordDictionary:
+#     def __init__(self):
+#         self.words = collections.defaultdict(set)
+    
+#     def addWord(self, word: str) -> None:
+#         self.words[len(word)].add(word)
+
+#     def search(self, word: str) -> bool:
+#         if word in self.words[len(word)]:
+#             return True 
+#         return any(re.search(word, match) for match in self.words[len(word)])
+
+
