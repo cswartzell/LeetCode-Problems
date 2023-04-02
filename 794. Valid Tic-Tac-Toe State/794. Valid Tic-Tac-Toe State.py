ef39fff6458c73c05f77dc489f368cd2fbb67f36
Cameron Swartzell <csswartzell@gmail.com>
2023-04-02 01:02:54 -0700

    03-31-2023

--- 794. Valid Tic-Tac-Toe State/794. Valid Tic-Tac-Toe State.py
+++ 794. Valid Tic-Tac-Toe State/794. Valid Tic-Tac-Toe State.py
@@ -1,21 +1,26 @@
+
 class Solution:
     def validTicTacToe(self, board: List[str]) -> bool:
+        turn_count = {"X": 0, "O": 0, " ":0}
+        for c in board[0] + board[1] + board[2]:
+            turn_count[c] += 1
 
-        sum(
-            board[0][0] == board[0][1] == board[0][2] == "X",
-            board[1][0] == board[1][1] == board[1][2] == "X",
-            board[2][0] == board[2][1] == board[2][2] == "X",
-            board[0][0] == board[0][1] == board[0][2] == "O",
-            board[1][0] == board[1][1] == board[1][2] == "O",
-            board[2][0] == board[2][1] == board[2][2] == "O",
-            board[0][0] == board[1][0] == board[2][0] == "X",
-            board[0][1] == board[1][1] == board[2][1] == "X",
-            board[0][2] == board[1][2] == board[2][2] == "X",
-            board[0][0] == board[1][0] == board[2][0] == "O",
-            board[0][1] == board[1][1] == board[2][1] == "O",
-            board[0][2] == board[1][2] == board[2][2] == "O",
-            board[0][0] == board[1][1] == board[2][2] == "X",
-            board[0][2] == board[1][1] == board[2][0] == "X",
-            board[0][0] == board[1][1] == board[2][2] == "O",
-            board[0][2] == board[1][1] == board[2][0] == "O",
-        )
+        def horizontal(player) -> bool:
+            return any(all(board[i][j] == player for j in range(len(board)) for i in range(len(board[0]))))
+        def vertical(player) -> bool:
+            return any(all(board[i][j] == player for i in range(len(board)) for j in range(len(board[0]))))
+        def diagonal(player) -> bool:
+            return any(all(board[i][i-j*(2*i+1)] == player for i in range(len(board)) for j in [0,1]))
+
+        X_wins = any(horizontal("X"), vertical("X"), diagonal("X"))
+        O_wins = any(horizontal("O"), vertical("O"), diagonal("O"))
+
+        if X_wins and O_wins:
+            return False
+        if X_wins:
+            return turn_count["X"] - turn_count["O"] == 1
+        if O_wins:
+            return turn_count["X"] - turn_count["O"] == 0
+        if turn_count["X"] - turn_count["O"] < 0 or turn_count["X"] - turn_count["O"] > 1:
+            return False
+        return True
